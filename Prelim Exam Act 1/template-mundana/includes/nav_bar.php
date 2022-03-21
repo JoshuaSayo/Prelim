12be421ba731440057fbc6da00644a397b5c58d1
BuildTools <unconfigured@null.spigotmc.org>
2022-03-21 15:01:30 +0800

    HTML-CSS

---
+++ Prelim Exam Act 1/template-mundana/includes/nav_bar.php
@@ -0,0 +1,45 @@
+<!--------------------------------------
+NAVBAR
+--------------------------------------->
+<nav class="topnav navbar navbar-expand-lg navbar-light bg-white fixed-top">
+<div class="container">
+	<a class="navbar-brand" href="./index.html"><strong>Mundana</strong></a>
+	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="navbar-collapse collapse" id="navbarColor02" style="">
+		<ul class="navbar-nav mr-auto d-flex align-items-center">
+			<li class="nav-item">
+			<a class="nav-link" href="./index.php">Intro <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="./article.php">Culture</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="./article.php">Tech</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="./article.php">Politics</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="./category.php">Health</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="./category.php">Collections</a>
+			</li>            
+			<li class="nav-item">
+			<a class="nav-link" href="./about.php">About</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="./docs.php">Template <span class="badge badge-secondary">docs</span></a>
+			</li>
+		</ul>
+		<ul class="navbar-nav ml-auto d-flex align-items-center">
+			<li class="nav-item highlight">
+			<a class="nav-link" href="https://www.wowthemes.net/mundana-free-html-bootstrap-template/">Get this Theme</a>
+			</li>
+		</ul>
+	</div>
+</div>
+</nav>
+<!-- End Navbar -->
